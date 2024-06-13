cc0848401ab8f2af3f8e5db526585acb79bacb00
Bhagya-4310 <157447593+Bhagya-4310@users.noreply.github.com>
2024-06-13 11:40:55 +0530

    commit1

---
+++ js.js
@@ -0,0 +1,5 @@
+function manipulatingStyles(){
+document.getElementById("headingElement").textContent = "4.0 techno";
+document.getElementById("headingElement").style.color = "blue";
+console.log("hello");
+}
